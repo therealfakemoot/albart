6cd66f0584ca68bec693be3f6144dd7c4a18c750
Nick Dumas <dumasn@homes.com>
2022-06-07 12:15:07 -0400

    Whew. Working background art.

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image/color"
 	"log"
+	// "math/rand"
 
 	"github.com/fogleman/gg"
+	"github.com/jdxyw/generativeart"
+	"github.com/jdxyw/generativeart/arts"
 	"github.com/nfnt/resize"
+
 	"github.com/therealfakemoot/albart"
 )
+
+/*
+func LogoAnchor(c *gg.Context, logo *gg.Context) gg.Point {
+	rand.Seed(8675309)
+	x, y := c.Width(), c.Height()
+	w, h := logo.Width(), logo.Height()
+
+	dir := []string{"N", "S", "E", "W"}
+	switch dir[rand.Int31n(int32(len(dir)))] {
+	case "W":
+		// xSample := rand.Int63n(int64(0.5*w) + 1)
+		bumper := .5 * w
+		xSample := rand.Int63n(int64(x)) + bumper
+	}
+
+	// xSample := rand.Float64() * x
+	// ySample := rand.Float64() * y
+}
+*/
 
 func main() {
 	var (
@@ -38,18 +62,29 @@ func main() {
 	// logoContext := gg.NewContext(
 	logoContext := gg.NewContextForImage(resize.Resize(uint(width/4), 0, logoFile, resize.Lanczos3))
 
-	baseContext := gg.NewContext(width, height)
+	colors := []color.RGBA{
+		{0x06, 0x7B, 0xC2, 0xFF},
+		{0x84, 0xBC, 0xDA, 0xFF},
+		{0xEC, 0xC3, 0x0B, 0xFF},
+		{0xF3, 0x77, 0x48, 0xFF},
+		{0xD5, 0x60, 0x62, 0xFF},
+	}
+	c := generativeart.NewCanva(width, height)
+	c.SetBackground(color.RGBA{0xF0, 0xFE, 0xFF, 0xFF})
+	c.FillBackground()
+	c.SetColorSchema(colors)
+	c.Draw(arts.NewNoiseLine(1000))
+	// c.ToPNG("fuck.png")
 
-	baseContext.SetRGB(0, 0, 0)
-	baseContext.Clear()
-	baseContext.SetRGB(1, 1, 1)
+	baseContext := gg.NewContextForRGBA(c.Img())
 	// dc.MeasureString() for adjusting text rendering
 	// DrawRoundedRectangle might be nice
 	corners := albart.LaneCorners(albart.North, width, height, logoContext.Width(), logoContext.Height())
 
 	anchor := albart.LaneAnchor(corners)
 
-	baseContext.DrawImageAnchored(logoContext.Image(), int(anchor.X)*2, int(anchor.Y), .5, .5)
+	// DrawImageAnchored draws the specified image at the specified anchor point. The anchor point is x - w * ax, y - h * ay, where w, h is the size of the image. Use ax=0.5, ay=0.5 to center the image at the specified point.
+	baseContext.DrawImageAnchored(logoContext.Image(), int(anchor.X), int(anchor.Y), .5, .5)
 
 	// need to rescale this to desired output dimensions
 	err = baseContext.SavePNG(outfile)
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/therealfakemoot/albart
 go 1.17
 
 require (
-	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/fogleman/gg v1.3.0
+	github.com/jdxyw/generativeart v0.0.0-20220127024657-50049f153090
+)
+
+require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
