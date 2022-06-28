ff9b05ebcb4e85008469354f813b1539de514df0
Nick Dumas <dumasn@homes.com>
2022-06-28 10:36:16 -0400

    Lifted palette code into the module
    now we can use it everywhere

--- cmd/gen-colors/main.go
+++ cmd/gen-colors/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
-	"fmt"
-	"image/color"
-	"io"
 	"log"
 	"os"
-	"strconv"
+
+	"github.com/therealfakemoot/albart"
 )
 
 func main() {
@@ -27,66 +24,7 @@ func main() {
 		log.Println("Couldn't open infile")
 		log.Fatal(err)
 	}
-	palettes, err := BuildPaletteFromFile(f)
+	palettes, err := albart.BuildPaletteFromFile(f)
 	log.Printf("color palette: %#v", palettes)
 
-}
-
-type Palette struct {
-	Name   string
-	Colors []color.Color
-}
-
-func BuildPaletteFromFile(r io.Reader) ([]Palette, error) {
-	p := make([]Palette, 0)
-
-	csvReader := csv.NewReader(r)
-
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, fmt.Errorf("Error reading record: %w", err)
-		}
-
-		log.Printf("Record found: %#v\n", record)
-		name, colors, err := ExtractRecord(record)
-		if err != nil {
-			return nil, fmt.Errorf("Error extracting palette from record: %w", err)
-		}
-		p = append(p, Palette{Name: name, Colors: colors})
-
-	}
-
-	return p, nil
-}
-
-func ExtractRecord(record []string) (string, []color.Color, error) {
-	name := record[0]
-	colors := make([]color.Color, 0)
-
-	for _, color := range record[1:] {
-		c, err := extractRGB(color)
-		if err != nil {
-			return "", nil, err
-		}
-		colors = append(colors, c)
-	}
-
-	return name, colors, nil
-}
-
-func extractRGB(hex string) (color.RGBA, error) {
-	rgb, err := strconv.ParseUint(hex, 16, 64)
-	if err != nil {
-		return color.RGBA{}, err
-	}
-
-	return color.RGBA{
-		R: uint8((rgb >> 16)),
-		G: uint8((rgb >> 8) & 0xFF),
-		B: uint8((rgb & 0xFF)),
-	}, nil
 }
---
+++ palette.go
@@ -0,0 +1,67 @@
+package albart
+
+import (
+	"encoding/csv"
+	"fmt"
+	"image/color"
+	"io"
+	"strconv"
+)
+
+type Palette struct {
+	Name   string
+	Colors []color.Color
+}
+
+func BuildPaletteFromFile(r io.Reader) ([]Palette, error) {
+	p := make([]Palette, 0)
+
+	csvReader := csv.NewReader(r)
+
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("Error reading record: %w", err)
+		}
+
+		name, colors, err := ExtractRecord(record)
+		if err != nil {
+			return nil, fmt.Errorf("Error extracting palette from record: %w", err)
+		}
+		p = append(p, Palette{Name: name, Colors: colors})
+
+	}
+
+	return p, nil
+}
+
+func ExtractRecord(record []string) (string, []color.Color, error) {
+	name := record[0]
+	colors := make([]color.Color, 0)
+
+	for _, color := range record[1:] {
+		c, err := extractRGB(color)
+		if err != nil {
+			return "", nil, err
+		}
+		colors = append(colors, c)
+	}
+
+	return name, colors, nil
+}
+
+func extractRGB(hex string) (color.RGBA, error) {
+	rgb, err := strconv.ParseUint(hex, 16, 64)
+	if err != nil {
+		return color.RGBA{}, err
+	}
+
+	return color.RGBA{
+		R: uint8((rgb >> 16)),
+		G: uint8((rgb >> 8) & 0xFF),
+		B: uint8((rgb & 0xFF)),
+	}, nil
+}
